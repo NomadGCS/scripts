94773b75530ffc182b39e56f2a57d22c7a215099
Mark <mark.vogel@nomadgcs.com>
2023-12-12 22:16:47 -0700

    Remove old shell scripts

--- clone-services.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --rm -it -w /app -v "$NTC_SERVICES_PATH:/app" \
-  -v "$SSH_AUTH_SOCK":/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent bitnami/git \
-  git clone https://github.com/NomadGCS/services.git
